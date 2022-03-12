684c83954db62beba0c17f3640287c32f54d1841
harrriii <kdlanguido@gmail.com>
2022-03-12 14:43:46 +0800

    test

--- index.php
+++ index.php
@@ -61,10 +61,11 @@ $_SESSION['access_level'] = 'admin';
         include_once 'src/pages/burger_machine/user/navbar.php';
         include_once 'src/pages/burger_machine/user/dashboard.php';
         include_once 'src/pages/burger_machine/user/ordering.php';
-        include_once 'src/pages/burger_machine/user/sidebar.php';
     }
     else if ($_SESSION['theme'] == 'burger_machine' && $_SESSION['access_level'] == 'admin') {
         include_once 'src/pages/burger_machine/admin/navbar.php';
+        include_once 'src/pages/burger_machine/admin/sidebar.php';
+
     }
     
 

47845bcd70dd25564718d5d819ce8ba482f512c6
syrulli <lloydlanguio@gmail.com>
2022-03-12 15:26:23 +0800

    navs

--- src/pages/burger_machine/user/navbar.php
+++ src/pages/burger_machine/user/navbar.php
@@ -13,7 +13,7 @@
         </div>
 
         <div class="nav__toggle">
-          <i class="fa-solid fa-bars icon_btn ps-3 pb-2" data-bs-toggle="offcanvas" href="#burger_machine_sidebar" role="button"></i>
+          <i class="fa-solid fa-bars icon_btn ps-3 pb-45" data-bs-toggle="offcanvas" href="#burger_machine_sidebar" role="button"></i>
 
         </div>
 
@@ -22,5 +22,20 @@
       </nav>
     </header>
 
+</div>
+
+<div class="offcanvas offcanvas-start burger_machine_sidebar" tabindex="-1" id="burger_machine_sidebar" aria-labelledby="burger_machine_sidebarLabel">
+    <div class="offcanvas-header">
+        <img src="<?php echo $_SESSION['logo_path'] ?>" class="burger_machine_nav_logo" />
+    </div>
+    <div class="offcanvas-body burger_machine_sidebar px-0">
+
+        <ul class="list-group list-group-flush" >
+            <li class="list-group-item fs-6 align-center"><i class="fa-solid fa-fw fa-house pe-3"></i> Home</a></li>
+            <li class="list-group-item fs-6 align-center"><i class="fa-solid fa-fw fa-house pe-3"></i> Contacts</a></li>
+            <li class="list-group-item fs-6 align-center"><i class="fa-solid fa-fw fa-house pe-3"></i> Carts</a></li>
+
+        </ul>
+    </div>
 </div>
 
--- src/pages/burger_machine/user/sidebar.php
+++ src/pages/burger_machine/user/sidebar.php
@@ -1,17 +1,15 @@
-<div class="offcanvas offcanvas-start burger_machine_sidebar" tabindex="-1" id="burger_machine_sidebar" aria-labelledby="burger_machine_sidebarLabel">
+<!-- <div class="offcanvas offcanvas-start burger_machine_sidebar" tabindex="-1" id="burger_machine_sidebar" aria-labelledby="burger_machine_sidebarLabel">
     <div class="offcanvas-header">
         <img src="<?php echo $_SESSION['logo_path'] ?>" class="burger_machine_nav_logo" />
     </div>
     <div class="offcanvas-body burger_machine_sidebar px-0">
 
         <ul class="list-group list-group-flush" >
-            <li class="list-group-item fs-2 align-center"><i class="fa-solid fa-lg fa-house pe-3"></i> Home</a></li>
-
-            <li class="list-group-item fs-2 align-center"><i class="fa-solid fa-lg fa-house pe-3"></i> Contacts</a></li>
-
-            <li class="list-group-item fs-2 align-center"><i class="fa-solid fa-lg fa-house pe-3"></i> Carts</a></li>
+            <li class="list-group-item fs-2 align-center"><i class="fa-solid fa-sm fa-house pe-3"></i> Home</a></li>
+            <li class="list-group-item fs-2 align-center"><i class="fa-solid fa-sm fa-house pe-3"></i> Contacts</a></li>
+            <li class="list-group-item fs-2 align-center"><i class="fa-solid fa-sm fa-house pe-3"></i> Carts</a></li>
 
           
         </ul>
     </div>
-</div>
+</div> -->

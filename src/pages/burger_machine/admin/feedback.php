f2a167bbe07889d401bbb2398adcf0c7ca1af1ea
harrriii <kdlanguido@gmail.com>
2022-03-12 16:11:37 +0800

    test

--- src/pages/burger_machine/admin/feedback.php
+++ src/pages/burger_machine/admin/feedback.php
@@ -1,12 +1,20 @@
-<div id="page_feedback" class="d-none page">
-<h1 class="text-white">FEEDBACK</h1>
+<div id="page_feedback" class=" page p-3">
+<h3 class="text-white pt-3">Feedbacks</h3>
 
-<table class="table">
+<table class="table_style_1 shadow">
     <thead>
-        <th>test</th>
+        <th class="text-center p-1 fw-normal">Name</th>
+        <th class="text-center p-1 fw-normal">Email</th>
+        <th class="text-center p-1 fw-normal">Phone Number</th>
+        <th class="text-center p-1 fw-normal">Feedback</th>
+        <th class="text-center p-1 fw-normal"></th>
     </thead>
     <tbody>
-        <td>test</td>
+        <td  class="text-center p-1">test</td>
+        <td  class="text-center p-1">test</td>
+        <td  class="text-center p-1">test</td>
+        <td  class="text-center p-1">test</td>
+        <td  class="text-center p-1"><i class="fa-solid fa-trash-can icon_btn text-danger"></i></td>
     </tbody>
 </table>
 

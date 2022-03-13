94e0c62cf2df44f02ee7543a0e426a57956b5620
harrriii <kdlanguido@gmail.com>
2022-03-13 13:44:47 +0800

    test

--- src/pages/burger_machine/admin/feedback.php
+++ src/pages/burger_machine/admin/feedback.php
@@ -1,13 +1,13 @@
 <div id="page_feedback" class=" page p-3">
-<h3 class="text-white pt-3">Feedbacks</h3>
+<h3 class="text-white pt-3" style=" font-family: Verdana, sans-serif;" >Feedbacks</h3>
 
-<table class="table_style_1 shadow">
-    <thead>
-        <th class="text-center p-1 fw-normal">Name</th>
-        <th class="text-center p-1 fw-normal">Email</th>
-        <th class="text-center p-1 fw-normal">Phone Number</th>
-        <th class="text-center p-1 fw-normal">Feedback</th>
-        <th class="text-center p-1 fw-normal"></th>
+<table class="table_style_1 shadow ">
+    <thead class="">
+        <th class="text-center p-1  rounded fw-normal">Name</th>
+        <th class="text-center p-1  rounded fw-normal">Email</th>
+        <th class="text-center p-1  rounded fw-normal">Phone Number</th>
+        <th class="text-center p-1  rounded fw-normal">Feedback</th>
+        <th class="text-center p-1  rounded fw-normal"></th>
     </thead>
     <tbody>
         <td  class="text-center p-1">test</td>

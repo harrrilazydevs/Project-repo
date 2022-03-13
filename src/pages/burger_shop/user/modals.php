8b0ecee4ecf9d81ec06a65c7c0747a1e80bcd124
syrulli <lloydlanguio@gmail.com>
2022-03-13 20:35:24 +0800

    check-out Modal

--- src/pages/burger_shop/user/modals.php
+++ src/pages/burger_shop/user/modals.php
@@ -75,7 +75,7 @@
                             <input type="Repeat Password" required>
                             <label>Repeat Password</label>
                         </div>
-                    
+
                         <div class="field text-center">
                             <button class="py-1 text-white" style="font-weight:bold; background:#F54646; border-radius:20px; width:120px; margin: 6px; border: 1px solid yellow;">Sign Up</button>
 
@@ -87,3 +87,71 @@
         </div>
     </div>
 <!-- SIGN UP MODAL -->
+
+<!-- CART MODAL -->
+<div class="modal fade " id="cartModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="text-center py-3 text-center" style="border-bottom:3px solid black;">
+                <span class="ms-5 h5 fw-bold"> Check-out </span>
+                <i class="fa-solid fa-circle-xmark icon_btn pe-3 fa-xl" style="float:right; padding-top:12px;" data-bs-dismiss="modal"></i>
+            </div>
+
+            <div class="wrapper-login">
+
+                <div class="row">
+                    <div class="col-7">
+                        <form action="#">
+                            <div class="field mt-5">
+                                <input type="text" required>
+                                <label>Email</label>
+                            </div>
+                            <div class="field">
+                                <input type="Full Name" required>
+                                <label>Full Name</label>
+                            </div>
+
+                            <div class="field">
+                                <input type="Address" required>
+                                <label>Address</label>
+                            </div>
+
+                            <div class="field">
+                                <input type="Phone No." required>
+                                <label>Phone No.</label>
+                            </div>
+
+                            <p class="ms-3 mt-2">List of orders: <small>Cheesy burgeritong Adobong malaman yea!</small></p>
+                            <p class="ms-3 mt-2">Total:_____</p>
+                        </form>
+                    </div>
+
+                    <div class="col-5" style="margin-top: 7%;">
+                        <p>Payment Method:</p>
+
+                        <div class="mb-3 form-check">
+                            <input type="checkbox" class="form-check-input" id="gcashCheckbox" style="border-radius: 50% !important;">
+                            <label class="form-check-label" for="gcashCheckbox">Gcash</label>
+                        </div>
+
+                        <div class="mb-3 form-check">
+                            <input type="checkbox" class="form-check-input" id="cashCheckbox"  style="border-radius: 50% !important;">
+                            <label class="form-check-label" for="cashCheckbox">Cash on Delivery</label>
+                        </div>
+
+                        <p>Note:</p><textarea name="" id="" cols="30" rows="5"></textarea>
+
+
+                        <button class="py-1 text-white" style="font-weight:bold; background:#F54646; border-radius:20px; width:120px; margin: 6px; border: 1px solid yellow;">Check-out</button>
+
+
+                    </div>
+
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+</div>
+<!-- CART MODAL -->

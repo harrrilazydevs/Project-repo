b179cae382d6c0e36c565fff74aa4a3cb55a1ece
harrriii <kdlanguido@gmail.com>
2022-03-13 18:24:34 +0800

    test

--- src/pages/burger_machine/user/viewMenu.php
+++ src/pages/burger_machine/user/viewMenu.php
@@ -1,5 +1,4 @@
 <div class="d-none page" id="page_viewMenu">
-
     <section class="mt-5" style="background-color: white; height: 300px;">
         <div class="container mt-5">
             <div class="row align-items-center text-center">
@@ -15,7 +14,7 @@
         <div class="container mt-4 mb-5">
             <div class="dropdown">
                 <button class="btn btn-secondary dropdown-toggle text-black" href="#" role="button" data-bs-toggle="dropdown" style="font-weight:bold; background:#FFD600; border-radius:25px; width:150px; border: none !important;">
-                    Burgers
+                    All Products
                 </button>
 
                 <ul class="dropdown-menu text-center" style="background: #E4DEBE; width: 15px;">

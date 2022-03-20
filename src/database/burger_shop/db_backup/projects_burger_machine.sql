fd3936aecb444ff860a61e5afb505eb500dcc6b9
harrrilazydevs <86334894+harrrilazydevs@users.noreply.github.com>
2022-03-20 19:34:45 +0800

    final
    test

--- src/database/burger_shop/db_backup/projects_burger_machine.sql
+++ src/database/burger_shop/db_backup/projects_burger_machine.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 17, 2022 at 11:54 AM
+-- Generation Time: Mar 20, 2022 at 12:00 PM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 7.4.15
 
@@ -30,10 +30,28 @@ SET time_zone = "+00:00";
 CREATE TABLE `tbl_feedbacks` (
   `id` int(11) NOT NULL,
   `feedback` text NOT NULL,
-  `name` text NOT NULL,
-  `email` text NOT NULL,
-  `phone` text NOT NULL
+  `user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_locations`
+--
+
+CREATE TABLE `tbl_locations` (
+  `id` int(11) NOT NULL,
+  `location` varchar(100) NOT NULL,
+  `price` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_locations`
+--
+
+INSERT INTO `tbl_locations` (`id`, `location`, `price`) VALUES
+(1, 'Muntinlupa', 50),
+(2, 'Outside Muntinlupa', 75);
 
 -- --------------------------------------------------------
 
@@ -46,22 +64,10 @@ CREATE TABLE `tbl_orders` (
   `user_id` int(11) NOT NULL,
   `product_id` int(11) NOT NULL,
   `qty` int(11) NOT NULL,
-  `email` text NOT NULL,
   `status` text NOT NULL,
   `ref_no` text NOT NULL,
-  `order_date` date DEFAULT NULL
+  `order_date` date DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `tbl_orders`
---
-
-INSERT INTO `tbl_orders` (`id`, `user_id`, `product_id`, `qty`, `email`, `status`, `ref_no`, `order_date`) VALUES
-(1, 1, 1, 1, '1', 'Completed', '1', '2022-03-15'),
-(2, 1, 2, 1, '1', 'Completed', '1', '2022-03-15'),
-(3, 1, 3, 1, '1', 'Completed', '1', '2022-03-15'),
-(4, 1, 1, 2, '1', 'Completed', '2', '2022-03-14'),
-(5, 1, 2, 1, '3', 'Completed', '2', '2022-03-14');
 
 -- --------------------------------------------------------
 
@@ -73,18 +79,14 @@ CREATE TABLE `tbl_payments` (
   `id` int(11) NOT NULL,
   `order_ref_no` varchar(50) NOT NULL,
   `payment_method` varchar(50) NOT NULL,
-  `payment_details` varchar(50) NOT NULL,
+  `payment_details` varchar(50) DEFAULT NULL,
   `payment` double NOT NULL,
-  `payment_date` date NOT NULL DEFAULT current_timestamp()
+  `payment_date` date NOT NULL DEFAULT current_timestamp(),
+  `note_to_rider` text NOT NULL,
+  `delivery_address` varchar(100) NOT NULL,
+  `delivery_fee` double NOT NULL,
+  `gcash_payment_proof` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `tbl_payments`
---
-
-INSERT INTO `tbl_payments` (`id`, `order_ref_no`, `payment_method`, `payment_details`, `payment`, `payment_date`) VALUES
-(1, '2', 'gcash', '090808909', 600.6, '2022-03-16'),
-(2, '1', 'gcash', '0905509055654', 900, '0000-00-00');
 
 -- --------------------------------------------------------
 
@@ -105,11 +107,17 @@ CREATE TABLE `tbl_products` (
 --
 
 INSERT INTO `tbl_products` (`id`, `name`, `category`, `picture`, `price`) VALUES
-(1, 'Boa Hancock\'s Watermelon Juice', 'drink', 'src/resources/img/watermelon.jpg', 150),
-(2, 'Nami\'s Buns Burger', 'burger', 'src/resources/img/orderDecoy.png', 500),
-(3, 'Robin\'s Coleslaw', 'sides', 'src/resources/img/coleslaw.jpg', 300),
-(6, 'New Product', '', 'uploaded/cd_lname_fname.PNG', 0),
-(7, 'Newer ', 'burger', '../../../uploaded/chmod_file2.PNG', 600);
+(46, 'Blackbeard slash Whitebeard Burger', 'burger', 'src/uploaded/Blackbeard slash Whitebeard Burger.jpg', 100),
+(47, 'Escanor Burger', 'burger', 'src/uploaded/Escanor Burger.jpg', 100),
+(48, 'Ichigo Burger', 'burger', 'src/uploaded/Ichigo Burger.jpg', 100),
+(50, 'Naruto Burger', 'burger', 'src/uploaded/Naruto Burger.jpg', 100),
+(51, 'Natsu Burger', 'burger', 'src/uploaded/Natsu Burger.jpg', 100),
+(52, 'Rengoku', 'sides', 'src/uploaded/rengoku.jpg', 80),
+(53, 'Meliodas Nachos', 'sides', 'src/uploaded/Meliodas Nachos.jpg', 150),
+(54, 'Saitama Burger', 'burger', 'src/uploaded/Saitama Burger.jpg', 150),
+(55, 'Sakuragi Burger', 'burger', 'src/uploaded/Sakuragi Burger.jpg', 100),
+(56, 'San Goku Burger', 'burger', 'src/uploaded/San Goku Burger.jpg', 100),
+(57, 'Zenitsu', 'burger', 'src/uploaded/zenitsu.jpg', 300);
 
 -- --------------------------------------------------------
 
@@ -125,15 +133,17 @@ CREATE TABLE `tbl_users` (
   `access_level` text NOT NULL,
   `phone` text NOT NULL,
   `name` text NOT NULL,
-  `register_date` date DEFAULT NULL
+  `register_date` date DEFAULT current_timestamp(),
+  `address` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `tbl_users`
 --
 
-INSERT INTO `tbl_users` (`id`, `username`, `email`, `password`, `access_level`, `phone`, `name`, `register_date`) VALUES
-(1, 'devt', 'dev@mail.com', '1', 'admin', '22222', 'Harri', '2022-03-01');
+INSERT INTO `tbl_users` (`id`, `username`, `email`, `password`, `access_level`, `phone`, `name`, `register_date`, `address`) VALUES
+(1, 'dev', 'dev@mail.com', '1', 'admin', '22222', 'Harri', '2022-03-01', '#21 Malakasan St Brgy Tibay '),
+(2, 'admin', 'admin@mail.com', '1', 'admin', '0000', 'Junior', '2022-03-01', '#21 Malakasan St Brgy Tibay ');
 
 --
 -- Indexes for dumped tables
@@ -143,6 +153,13 @@ INSERT INTO `tbl_users` (`id`, `username`, `email`, `password`, `access_level`, 
 -- Indexes for table `tbl_feedbacks`
 --
 ALTER TABLE `tbl_feedbacks`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `tbl_locations`
+--
+ALTER TABLE `tbl_locations`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -170,7 +187,8 @@ ALTER TABLE `tbl_products`
 -- Indexes for table `tbl_users`
 --
 ALTER TABLE `tbl_users`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `address` (`address`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -180,35 +198,47 @@ ALTER TABLE `tbl_users`
 -- AUTO_INCREMENT for table `tbl_feedbacks`
 --
 ALTER TABLE `tbl_feedbacks`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `tbl_locations`
+--
+ALTER TABLE `tbl_locations`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `tbl_orders`
 --
 ALTER TABLE `tbl_orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=98;
 
 --
 -- AUTO_INCREMENT for table `tbl_payments`
 --
 ALTER TABLE `tbl_payments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 
 --
 -- AUTO_INCREMENT for table `tbl_products`
 --
 ALTER TABLE `tbl_products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=58;
 
 --
 -- AUTO_INCREMENT for table `tbl_users`
 --
 ALTER TABLE `tbl_users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `tbl_feedbacks`
+--
+ALTER TABLE `tbl_feedbacks`
+  ADD CONSTRAINT `tbl_feedbacks_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `tbl_users` (`id`);
 
 --
 -- Constraints for table `tbl_orders`

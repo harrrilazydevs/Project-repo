a0b723001d1fc9cfa61dcca4d2e47aa4af6ff4a6
harrrilazydevs <86334894+harrrilazydevs@users.noreply.github.com>
2022-03-30 20:54:59 +0800

    try revert

---
+++ src/func/dental_clinic/admin/admin.js
@@ -0,0 +1,23 @@
+let page = {
+    overview : "page_overview",
+    registeredPatient : "page_registeredPatient",
+    history : "page_history",
+};
+
+function change_page(pagename) {
+  $('.page').addClass('d-none');
+  $('#' + page[pagename]).removeClass('d-none');
+}
+
+
+$('.btn_view_history').on('click', function(){
+    change_page('history')
+})
+$('.btn_view_overview').on('click', function(){
+    change_page('overview')
+})
+
+$('.btn_view_registeredPatient').on('click', function(){
+    change_page('registeredPatient')
+})
+
---
+++ src/pages/dental_clinic/admin/admin_modal.php
@@ -0,0 +1,24 @@
+<div class="modal fade" id="admin_appointment_upload" aria-hidden="true" style="backdrop-filter: blur(16px) saturate(132%); -webkit-backdrop-filter: blur(16px) saturate(132%); background-color: rgba(17, 25, 40, 0.02);">
+    <div class=" modal-dialog">
+        <div class="modal-content" style="border-radius: 5%;">
+            <div class="modal-body text-black">
+                <div class="container" style="border-radius: 5%; border: solid grey 5px;">
+                    <h5 class="text-center mt-3 mb-6" style="letter-spacing: 2px; font-family: 'Work Sans', sans-serif; font-weight: bold;">UPLOAD</h5>
+                    <p><small class="text-muted ms-2">Upload patient's laboratory results.</small></p>
+                    <div class="row text-center">
+                        <div class="col-12">
+                            <img src="src/resources/img/upload.png" alt="">
+                        </div>
+                    </div>
+
+                    <div class="row text-center mt-4 mb-4">
+                        <div class="col-12">
+                            <button class="text-white me-4"  style="background:#80CEB8; border-radius:5px; border: none; cursor: pointer; font-size: 12px; width: 100px; height: 2rem;">UPLOAD</button>
+                            <button class="text-white" data-bs-dismiss="modal" style="background:#80CEB8; border-radius:5px; border: none; cursor: pointer; font-size: 12px; width: 100px; height: 2rem;">BACK</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>

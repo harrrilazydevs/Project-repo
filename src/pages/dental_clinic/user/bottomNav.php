42afbbdeb82fbb6d7e17f552f9b1ecb50ecf4643
SyrulliLazyDev <79763710+Syrulli@users.noreply.github.com>
2022-04-07 20:25:15 +0800

    Bottom Navbar addded (USER)
    Bottom Navbar addded (USER)

---
+++ src/pages/dental_clinic/user/bottomNav.php
@@ -0,0 +1,23 @@
+<nav class="bottom_nav_menu  d-sm-block d-md-none">
+    <ul class="bottom_nav_list">
+        <li class="bottom_nav_item">
+            <i title="View Dashboard" class="fa-fw fa-solid fa-house-chimney"></i>
+        </li>
+
+        <li class="bottom_nav_item">
+            <i title="View Incoming Appointments" class="fa-fw fa-solid fa-calendar"></i>
+        </li>
+
+        <li class="bottom_nav_item">
+            <i title="View Laboratory Results" class="fa-fw fa-solid fa-folder-closed"></i>
+        </li>
+
+        <li class="bottom_nav_item">
+            <i title="View Appointments" class="fa-fw fa-solid fa-clock-rotate-left"></i>
+        </li>
+        
+        <li class="bottom_nav_item">
+            <i title="Logout" class="fa-fw fa-solid fa-arrow-right-from-bracket icon_btn"></i>
+        </li>
+    </ul>
+</nav>
